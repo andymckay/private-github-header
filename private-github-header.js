834575ce8b2ec4febb346d8753e47fd9459e828c
Andy McKay <andymckay@github.com>
2021-08-26 16:32:10 -0700

    Update private-github-header.js

--- private-github-header.js
+++ private-github-header.js
@@ -1,7 +1,7 @@
 // Decide if this is a private repo by looking for a Label with the text "Private"
 function isPrivateRepo() {
   var outlineLabels = document.querySelectorAll(
-    "span.Label.Label--outline.v-align-middle"
+    "span.Label.Label--secondary.v-align-middle"
   );
   for (const label of outlineLabels) {
     if (label.innerText == "Private") {
@@ -11,7 +11,7 @@ function isPrivateRepo() {
   return false;
 }
 
-if (isPrivateRepo()) {
+if (!isPrivateRepo()) {
   // Set the background color of the header to dark red.
   document.querySelectorAll("header")[0].style.backgroundColor = "darkRed";
 }
